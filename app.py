faede283b7608183cd2fb00e5602e5bf69684c5e
BrightProgrammer7 <abdelilahk627@gmail.com>
2023-07-29 00:55:54 +0100

    last commit

---
+++ app.py
@@ -0,0 +1,15 @@
+def Hello(name):
+    print('Hello', name)
+
+def power(a,n):
+    return pow(a,n)
+
+def multiply(a,b):
+    return a*b
+
+def square(a):
+    return a*a
+
+# def squart(a):
+#     from math import *
+#     return a.sqrt()
